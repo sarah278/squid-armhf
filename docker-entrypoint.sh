180d5757d05971c22a146138936142412a782c82
Sarah Kuhn <sarah_kuhn@live.de>
2018-01-13 20:29:30 +0100

    Create docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# ----------------------------------------------------------------------------
+# entrypoint for squid container
+# ----------------------------------------------------------------------------
+set -e
+
+SQUID_VERSION=$(/usr/sbin/squid -v | grep Version | awk '{ print $4 }')
+if [ "$1" == "squid" ]; then
+  echo "Staring squid [${SQUID_VERSION}]"
+  chown -R squid:squid /var/cache/squid
+  exec /sbin/su-exec root /usr/sbin/squid -N
+else
+  exec "$@"
+fi
